aedcef4dd4911fe02f56451ee12cd495b1c0c01c
Wrderly <97280890+Wrderly@users.noreply.github.com>
2023-07-05 00:45:32 +0800

    数据库自动配置
    完成了一个数据库自动初始化配置功能的部分

--- Test.cs
+++ Test.cs
@@ -13,8 +13,9 @@ namespace Database_CourseDesign
 {
     internal class Test
     {
-        private string connectionString;
+        private string connectionStr;
         private string path;
+        private string userID;
         public Test(string programPath)
         { 
             path = programPath;
@@ -22,36 +23,72 @@ namespace Database_CourseDesign
             string filePath = Path.Combine(path, "config.json");
             string jsonFromFile = File.ReadAllText(filePath);
             var configFromFile = JsonSerializer.Deserialize<Dictionary<string, string>>(jsonFromFile);
-            connectionString = configFromFile["connectionString"];
+            //读取配置文件
+            string dataSourse = configFromFile["dataSource"];
+            //使用SYSTEM登录
+            userID = configFromFile["systemID"];
+            string userPwd = configFromFile["systemPwd"];
+            connectionStr = "DATA SOURCE=" + dataSourse + ";" +
+                            "USER ID='\"" + userID + "\"';" +
+                            "PASSWORD='" + userPwd + "'";
         }
 
         public void ConnectTest()
         {
-            Console.WriteLine("ConnectTest start");
-            Console.WriteLine("Get Connection start");
+            Console.WriteLine(userID + " 开始获取数据库连接");
             //根据连接配置创建数据库连接
-            OracleConnection connection = new OracleConnection(connectionString);
-            Console.WriteLine("Get Connection end");
+            OracleConnection systemConnection = new OracleConnection(connectionStr);
+            Console.WriteLine(userID + " 获取数据库连接成功");
 
             try
             {
-                Console.WriteLine("Try start");
+                Console.WriteLine(userID + " 开始连接数据库");
                 //启动数据库连接
-                connection.Open();
-                Console.WriteLine("Connection successful!");
+                systemConnection.Open();
+                Console.WriteLine(userID + " 连接数据库成功");
             }
             catch (OracleException ex)
             {
-                Console.WriteLine("Connection failed: " + ex.Message);
+                Console.WriteLine(userID + " 连接数据库失败: " + ex.Message);
+                return;
             }
-            finally
+
+            Console.WriteLine(userID + " 关闭数据库连接");
+            //关闭数据库连接
+            systemConnection.Close();
+        }
+
+        public void PrintTableTest()
+        {
+            Console.WriteLine(userID + " 开始获取数据库连接");
+            //根据连接配置创建数据库连接
+            OracleConnection systemConnection = new OracleConnection(connectionStr);
+            Console.WriteLine(userID + " 获取数据库连接成功");
+
+            try
             {
-                Console.WriteLine("Close start");
-                //关闭数据库连接
-                connection.Close();
+                Console.WriteLine(userID + " 开始连接数据库");
+                //启动数据库连接
+                systemConnection.Open();
+                Console.WriteLine(userID + " 连接数据库成功");
+            }
+            catch (OracleException ex)
+            {
+                Console.WriteLine(userID + " 连接数据库失败: " + ex.Message);
+                return;
             }
 
+            string SQLStr = "SELECT table_name FROM user_tables";
+            OracleCommand command = new OracleCommand(SQLStr, systemConnection);
+            OracleDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader.GetString(0));
+            }
 
+            Console.WriteLine(userID + " 关闭数据库连接");
+            //关闭数据库连接
+            systemConnection.Close();
         }
     }
 }
